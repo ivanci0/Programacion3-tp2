4a513521fda7f32fd256a6d1c27cb01b0d5dc0ee
ivanci0 <unbaigorria.ivan@gmail.com>
2017-04-23 18:21:04 -0300

    Commit inicial

---
+++ tp02/Program.cs
@@ -0,0 +1,36 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tp02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("bienvenido, ingrese el nombre de la ciudad y luego enter");
+            string titulo = Console.ReadLine();
+
+            WebRequest req = WebRequest.Create($"https://maps.googleapis.com/maps/api/geocode/json?address={titulo.ToLower()}");
+
+            WebResponse respuesta = req.GetResponse();
+
+            Stream stream = respuesta.GetResponseStream();
+
+            StreamReader sr = new StreamReader(stream);
+
+            JObject data = JObject.Parse(sr.ReadToEnd());
+
+            string response = (string)data["Year"];
+
+            Console.WriteLine($"El año es: {response}");
+
+            Console.ReadLine();
+        }
+    }
+}
